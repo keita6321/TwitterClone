b37670d7dd20ffd19fe7803ca7e9fcd55f7f97c0
keita.koga <keita6321@gmail.com>
2017-04-20 02:27:19 +0000

    final try twitter

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+
+  root to: 'toppages#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+
+  #get 'toppages/index'
+  #root to: 'tasks#index'
+  resources :tasks
+end
